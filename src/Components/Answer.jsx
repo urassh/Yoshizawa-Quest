223e38497a20c5ad0a109f2207eb15ae8c9414b9
urassh <shuto.1011@icloud.com>
2023-01-26 12:09:18 +0900

    状態変数quizを廃止。 状態変数isCorrect を定義した。

--- src/Components/Answer.jsx
+++ src/Components/Answer.jsx
@@ -9,20 +9,15 @@ import correctImage from "../Images/correct.png";
 import incorrectImage from "../Images/incorrect.png"
 
 const Answer = ()=>{
-    const test_quiz_instance = QUESTIONS[Quiz.index];
+    const quizInstance = QUESTIONS[Quiz.index];
 
     const navigate = useNavigate();
-    const [quiz, setQuiz] = useState({
-        index: Quiz.index,
-        isCorrect: false,
-        correctCount: Quiz.correctCount,
-        totalPoint: Quiz.totalPoint,
-    });
+
+    const [isCorrect, setIsCorrect] = useState(false);
     const [showResultModal, setshowResultModal] = useState(false);
 
     const quizHandler = (e)=> {
         const answered = e.currentTarget.id;
-        console.log(`呼ばれたQuizインスタンス : ${test_quiz_instance.text}`);
         quizJudge(answered);
         setshowResultModal(true);
     }
@@ -30,27 +25,23 @@ const Answer = ()=>{
     const nextQuiz = () => {
         setshowResultModal(false);
         Quiz.index += 1;
-        if ((quiz.index+1) % Quiz.quizLength === 0){
+        if (Quiz.index % Quiz.quizLength === 0){
             Quiz.stage += 1;
-            navigate('/Yoshizawa-Quest/result', {state: {correct: quiz.correctCount, point: quiz.totalPoint}});
+            navigate('/Yoshizawa-Quest/result');
         } else {
-            setQuiz({...quiz, index: Quiz.index, isCorrect: false});
+            setIsCorrect(false);
         }
     }
 
     const quizJudge = (answerButtonIndex) => {
-        const correct = test_quiz_instance.correct;
+        const correct = quizInstance.correct;
         const answer = Number(answerButtonIndex);
         if(answer === correct) {
             Quiz.correctCount+=1;
-            Quiz.totalPoint += test_quiz_instance.point;
-            setQuiz({...quiz,
-                isCorrect: true,
-                correctCount: Quiz.correctCount,
-                totalPoint: Quiz.totalPoint
-            });
+            Quiz.totalPoint += quizInstance.point;
+            setIsCorrect(true);
         } else {
-            setQuiz({...quiz, isCorrect: false});
+            setIsCorrect(false);
         }
     }
 
@@ -64,7 +55,7 @@ const Answer = ()=>{
 
     const showDetailAnswer = (isCorrect) => {
         if(!(isCorrect)) {
-            return <h2>答えは{test_quiz_instance.correct}番の<br/>「{test_quiz_instance.buttonsList[test_quiz_instance.correct-1]}」です。</h2>;
+            return <h2>答えは{quizInstance.correct}番の<br/>「{quizInstance.buttonsList[quizInstance.correct-1]}」です。</h2>;
         } 
     }
 
@@ -72,28 +63,28 @@ const Answer = ()=>{
         <div className="quiz-container">
             <Modal isOpen={showResultModal}>
                 <div id="result-form">
-                    {showResultModalImage(quiz.isCorrect)}
-                    {showDetailAnswer(quiz.isCorrect)}
+                    {showResultModalImage(isCorrect)}
+                    {showDetailAnswer(isCorrect)}
                     <Button onClick={nextQuiz}>次へ</Button>
                 </div>
             </Modal>
-            <h1>第 {quiz.index % Quiz.quizLength+1} 問</h1>
-            <h3 id="level">レベル{test_quiz_instance.level}</h3>
+            <h1>第 {Quiz.index % Quiz.quizLength+1} 問</h1>
+            <h3 id="level">レベル{quizInstance.level}</h3>
             {/* <p id="quiz-text">
                 {settingQuetions[quiz.index].text}
             </p> */}
             <div className="answer-buttons">
                 <button id="1" className="answer-button button-1" onClick={quizHandler}>
-                    {test_quiz_instance.buttonsList[0]}
+                    {quizInstance.buttonsList[0]}
                 </button>
                 <button id="2" className="answer-button button-2" onClick={quizHandler}>
-                    {test_quiz_instance.buttonsList[1]}
+                    {quizInstance.buttonsList[1]}
                 </button>
                 <button id="3" className="answer-button button-3" onClick={quizHandler}>
-                    {test_quiz_instance.buttonsList[2]}
+                    {quizInstance.buttonsList[2]}
                 </button>
                 <button id="4" className="answer-button button-4" onClick={quizHandler}>
-                    {test_quiz_instance.buttonsList[3]}
+                    {quizInstance.buttonsList[3]}
                 </button> 
             </div>
         </div>
